79fd3147e1ea040b7e4d2e37e3804bb694270061
davidicus <david@david-conner.com>
2018-10-29 21:49:15 -0500

    :green_heart: remove babelrc

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * This function does something.
- * @param input Some param description.
- * @param options An object of options.
- * @returns something userful.
- */
-module.exports = (input, options = {}) => {};
